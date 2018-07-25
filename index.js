fd1525f881d786dc75baf39a4b8e21fa58eae22e
zivbrachia@gmail.com <zivbrachia@gmail.com>
2018-07-25 05:08:14 +0300

    server

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict'
+
+let express = require('express')
+let bodyParser = require('body-parser');
+let app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+let port = process.env.PORT || 5555;
+
+app.get('/', (req, res, next) => {
+    res.json({"result": "ok"});
+});
+app.post('/webhook', (req, res, next) => {
+    res.json({"result": "ziv"});
+});
+
+app.listen(port);
+
+console.log('Your application is running on http://localhost:' + port);
